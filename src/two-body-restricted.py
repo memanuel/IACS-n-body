004f28b361f845d7fa62f722034929485b3b2bf5
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-06-18 17:20:42 -0400

    two body restricted in progress

---
+++ src/two-body-restricted.py
@@ -0,0 +1,13 @@
+"""
+Harvard IACS Masters Thesis
+Two Body Problem (Restricted)
+
+Michael S. Emanuel
+Tue Jun  18 15:29 2019
+"""
+
+import numpy as np
+
+# Local imports
+from utils import arange_inc
+
