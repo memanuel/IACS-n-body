6b0c989930c7d21a0cb9ab924b60e5fd3047b559
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-06-19 21:58:16 -0400

    migrated code from ipynb to two_body_restricted.py

---
+++ src/two_body_restricted.py
@@ -0,0 +1,318 @@
+"""
+Harvard IACS Masters Thesis
+Two Body Problem (Restricted)
+
+Michael S. Emanuel
+Tue Jun  18 15:29 2019
+"""
+
+# Library imports
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Aliases
+keras = tf.keras
+
+# Local imports
+import utils
+
+# ********************************************************************************************************************* 
+def make_traj_r2bc(r, theta0, n_years):
+    """
+    Make an array of training data points for the restricted 2 body circular problem.
+    Creates one trajectory with the same initial configuration.
+    INPUTS:
+    r: The radius of the orbit in AU
+    theta0: the angle of the planet in the ecliptic at time 0 years
+    n_years: the number of years of data to simulate
+    RETURNS:
+    input_dict: a dictionary with the input fields t, q0, v0
+    output_dict: a dictionary with the output fields q, v, a
+    """
+    # The sample frequency is 365 per year (dt approximately = 1 day)
+    sample_freq = 365
+    # Number of samples including both start and end
+    N = sample_freq*n_years + 1
+    # Array of sample times at the specified frequency over the specified length of time
+    t = np.arange(N, dtype=np.float32) / sample_freq
+
+    # The gravitational constant in units of (AU, years, solar_mass)
+    G = (2.0 * np.pi)**2
+    # The mass of the sun in this coordinate system
+    m0 = 1.0
+    # The gravitational coefficient G*(m0 + m1) for the restricted two body problem in these units
+    mu = G * m0
+
+    # Set the angular frequency omega based on the radius using the relationship
+    # mu = omega^2 r^3 --> omega = sqrt(mu / r^3)
+    omega = np.sqrt(mu / r**3)
+    # Theta is offset by theta0
+    theta = (omega * t) + theta0
+
+    # Compute qx and qy from r and theta; wrap into position q
+    qx = r*np.cos(theta, dtype=np.float32)
+    qy = r*np.sin(theta, dtype=np.float32)
+    q = np.stack([qx, qy], axis=1)
+    
+    # Compute vx and vy; wrap into velocity v
+    vx = -omega * qy
+    vy = +omega * qx
+    v = np.stack([vx, vy], axis=1)
+
+    # Compute ax and ay; wrap into acceleration a
+    ax = -omega**2 * qx
+    ay = -omega**2 * qy
+    a = np.stack([ax, ay], axis=1)
+
+    # Repeat vectors for the initial position q0
+    qx0 = np.ones(N) * qx[0]
+    qy0 = np.ones(N) * qy[0]
+    q0 = np.stack([qx0, qy0], axis=1)
+    
+    # Repeat vectors for the initial velocity v0
+    vx0 = np.ones(N) * vx[0]
+    vy0 = np.ones(N) * vy[0]
+    v0 = np.stack([vx0, vy0], axis=1)
+    
+    # Assemble the input dict
+    inputs = {
+        't': t,
+        'q0': q0,
+        'v0': v0}
+
+    # Assemble the output dict
+    outputs = {
+        'q': q,
+        'v': v,
+        'a': a,
+        # also include the initial conditions, which should be recovered
+        'q0_rec': q0,
+        'v0_rec': v0}
+
+    # Return the dicts
+    return (inputs, outputs)
+
+
+# ********************************************************************************************************************* 
+def make_train_r2bc(n_traj: int, n_years: int, r_min: float = 0.25, r_max: float = 32.0, seed = 42):
+    """
+    Make a set of training data for the restricted two body problem
+    INPUTS:
+    n_traj: the number of trajectories to sample
+    n_years: the number of years for each trajectory, e.g. 2
+    r_min: minimal distance in AU, e.g. 0.25
+    r_max: maximal distance in AU, e.g. 32.0    
+    """
+    # The sample frequency is 365 per year (dt approximately = 1 day)
+    sample_freq = 365
+    # Number of samples including both start and end in each trajectory
+    traj_size = sample_freq*n_years + 1
+    # Total size of the output
+    n_rows = n_traj * traj_size
+
+    # Initialize six arrays for the data
+    vec_shape = (n_rows, 2)
+    t = np.zeros(n_rows, dtype=np.float32)
+    q0 = np.zeros(vec_shape, dtype=np.float32)
+    v0 = np.zeros(vec_shape, dtype=np.float32)
+    q = np.zeros(vec_shape, dtype=np.float32)
+    v = np.zeros(vec_shape, dtype=np.float32)
+    a = np.zeros(vec_shape, dtype=np.float32)
+    
+    # Set random seed for reproducible results
+    np.random.seed(seed=seed)
+
+    # Sample the trajectories
+    for i in range(n_traj):
+        # Sample r
+        r = np.random.uniform(low=r_min, high=r_max)
+        # Sample theta0
+        theta0 = np.random.uniform(low=-np.pi, high=np.pi)
+        # Generate one trajectory
+        inputs, outputs = make_traj_r2bc(r=r, theta0=theta0, n_years=n_years)
+        
+        # Range of rows for this trajectory
+        j0 = (i+0) * traj_size
+        j1 = (i+1) * traj_size
+        # Copy results into main arrays
+        t[j0:j1] = inputs['t']
+        q0[j0:j1, :] = inputs['q0']
+        v0[j0:j1, :] = inputs['v0']
+        q[j0:j1, :] = outputs['q']
+        v[j0:j1, :] = outputs['v']
+        a[j0:j1, :] = outputs['a']
+
+    # Assemble the input dict
+    inputs = {
+        't': t,
+        'q0': q0,
+        'v0': v0}
+
+    # Assemble the output dict
+    outputs = {
+        'q': q,
+        'v': v,
+        'a': a,
+        # also include the initial conditions, which should be recovered
+        'q0_rec': q0,
+        'v0_rec': v0}
+    
+    # Return the dicts
+    return (inputs, outputs)
+
+# ********************************************************************************************************************* 
+def plot_orbit_q(data):
+    """Plot the orbit position in a training sample"""
+    # Unpack data
+    t = data['t']
+    q = data['q']
+    qx = q[:, 0]
+    qy = q[:, 1]
+    # Compute the distance r
+    r = np.linalg.norm(q, axis=1)
+
+    # Plot the x and y coordinate
+    fig, ax = plt.subplots(figsize=[16, 9])
+    ax.set_title('Orbit Position')
+    ax.set_xlabel('t - years')
+    ax.set_ylabel('q - AU')
+    ax.set_xticks(np.arange(0.0, np.max(t)+0.25, 0.25))
+    ax.plot(t, qx, color='b', label='qx')
+    ax.plot(t, qy, color='r', label='qy')
+    ax.plot(t, r,  color='purple', label='r')
+    ax.grid()
+    ax.legend()
+    
+    return fig, ax
+
+# ********************************************************************************************************************* 
+def plot_orbit_v(data):
+    """Plot the orbit velocity in a training sample"""
+    # Unpack data
+    t = data['t']
+    v = data['v']
+    vx = v[:, 0]
+    vy = v[:, 1]
+    spd = np.linalg.norm(v, axis=1)
+    
+    # Plot the x and y coordinate
+    fig, ax = plt.subplots(figsize=[16, 9])
+    ax.set_title('Orbit Velocity')
+    ax.set_xlabel('t - years')
+    ax.set_ylabel('v - AU / year')
+    ax.set_xticks(np.arange(0.0, np.max(t)+0.25, 0.25))
+    ax.plot(t, vx, color='b', label='vx')
+    ax.plot(t, vy, color='r', label='vy')
+    ax.plot(t, spd, color='purple', label='spd')
+    ax.grid()
+    ax.legend()
+
+    return fig, ax
+
+# ********************************************************************************************************************* 
+def plot_orbit_a(data):
+    """Plot the orbit acceleration in a training sample"""
+    # Unpack data
+    t = data['t']
+    a = data['a']
+    ax = a[:, 0]
+    ay = a[:, 1]
+    acc = np.linalg.norm(a, axis=1)
+    
+    # Plot the x and y coordinate
+    # Name the axes object ax_ rather than ax to avoid a name collision with the x component of acceleration, ax
+    fig, ax_ = plt.subplots(figsize=[16, 9])
+    ax_.set_title('Orbit Acceleration')
+    ax_.set_xlabel('t - years')
+    ax_.set_ylabel('a - $AU / year^2$')
+    ax_.set_xticks(np.arange(0.0, np.max(t)+0.25, 0.25))
+    ax_.plot(t, ax, color='b', label='ax')
+    ax_.plot(t, ay, color='r', label='ay')
+    ax_.plot(t, acc, color='purple', label='acc')
+    ax_.grid()
+    ax_.legend()
+
+    return fig, ax_
+
+# ********************************************************************************************************************* 
+def plot_orbit_energy(data):
+    """Plot the orbit energy in a training sample"""
+    # Unpack data
+    t = data['t']
+    q = data['q']
+    v = data['v']
+    r = np.linalg.norm(q, axis=1)
+    spd = np.linalg.norm(v, axis=1)
+    
+    # Compute the kinetic energy over m1
+    T = 0.5 * spd * spd
+    
+    # Compute the potential energy over m1
+    mu = (2.0 * np.pi)**2
+    U = -mu / r
+    
+    # The total energy
+    E = T + U
+    
+    # Plot the x and y coordinate
+    fig, ax = plt.subplots(figsize=[16, 9])
+    ax.set_title('Orbit Energy')
+    ax.set_xlabel('t - years')
+    ax.set_ylabel('Energy / m1 in $(au/year)^2$')
+    ax.set_xticks(np.arange(0.0, np.max(t)+0.25, 0.25))
+    ax.plot(t, T, color='b', label='T')
+    ax.plot(t, U, color='r', label='U')
+    ax.plot(t, E, color='purple', label='E')
+    ax.grid()
+    ax.legend()
+
+    return fig, ax
+
+# ********************************************************************************************************************* 
+def make_datasets_earth():
+    """Make 3 data sets for earth-like orbits with a=1"""
+    # Generate inputs and outputs for the earth's orbit
+    inputs_earth_trn, outputs_earth_trn = make_train_r2bc(n_traj=100, n_years=2, r_min=1.0, r_max=1.0, seed=42)
+    inputs_earth_val, outputs_earth_val = make_train_r2bc(n_traj=20, n_years=2, r_min=1.0, r_max=1.0, seed=43)
+    inputs_earth_tst, outputs_earth_tst = make_train_r2bc(n_traj=20, n_years=2, r_min=1.0, r_max=1.0, seed=44)
+    
+    # Create DataSet objects for toy size problem - earth orbits only (a=1, e=0)
+    ds_earth_trn = tf.data.Dataset.from_tensor_slices((inputs_earth_trn, outputs_earth_trn))
+    ds_earth_val = tf.data.Dataset.from_tensor_slices((inputs_earth_val, outputs_earth_val))
+    ds_earth_tst = tf.data.Dataset.from_tensor_slices((inputs_earth_tst, outputs_earth_tst))
+    
+    # Set buffer and batch sizes
+    buffer_size = 16536
+    batch_size = 256
+    
+    # Shuffle and batch data sets
+    ds_earth_trn = ds_earth_trn.shuffle(buffer_size=buffer_size).batch(batch_size)
+    ds_earth_val = ds_earth_val.shuffle(buffer_size=buffer_size).batch(batch_size)
+    ds_earth_tst = ds_earth_tst.shuffle(buffer_size=buffer_size).batch(batch_size)    
+    
+    # Return the three dataset objects
+    return ds_earth_trn, ds_earth_val, ds_earth_tst
+
+def make_datasets_solar():
+    """Make 3 data sets for typical solar system orbits with a in [0.25, 32.0]"""
+    # Generate inputs and outputs for orbits with r between 0.25 adn 32.0
+    inputs_trn, outputs_trn = make_train_r2bc(n_traj=10000, n_years=2, r_min=0.25, r_max=32.0, seed=42)
+    inputs_val, outputs_val = make_train_r2bc(n_traj=2000, n_years=2, r_min=0.25, r_max=32.0, seed=43)
+    inputs_tst, outputs_tst = make_train_r2bc(n_traj=2000, n_years=2, r_min=0.25, r_max=32.0, seed=44)
+    
+    # Create DataSet objects for train, val and test sets
+    ds_trn = tf.data.Dataset.from_tensor_slices((inputs_trn, outputs_trn))
+    ds_val = tf.data.Dataset.from_tensor_slices((inputs_val, outputs_val))
+    ds_tst = tf.data.Dataset.from_tensor_slices((inputs_tst, outputs_tst))
+    
+    # Set shuffle and batch size
+    buffer_size = 16536
+    batch_size = 256
+    
+    # Shuffle and batch data sets
+    ds_trn = ds_trn.shuffle(buffer_size=buffer_size).batch(batch_size)
+    ds_val = ds_val.shuffle(buffer_size=buffer_size).batch(batch_size)
+    ds_tst = ds_tst.shuffle(buffer_size=buffer_size).batch(batch_size)
+    
+    return ds_trn, ds_val, ds_tst
