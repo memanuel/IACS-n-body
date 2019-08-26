bec67717a9e9d3c757bfb6e9c17813b6e08a66e0
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-08-26 11:39:28 -0400

    removed link to data directory from repo

--- src/asteroid_traj.py
+++ src/asteroid_traj.py
@@ -6,3 +6,14 @@ Michael S. Emanuel
 Fri Aug 23 16:13:28 2019
 """
 
+# Magic command to run this file using remote kernel
+# %run "/d/IACS/n-body/src/asteroid_traj.py"
+
+import rebound
+from sej_data import make_sim_horizons
+
+
+# Create simulation using horizons
+sim = rebound.Simulation()
+object_names = ['Sun', 'Ceres']
+horizon_date = '2000-01-01 12:00'
