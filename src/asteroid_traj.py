2893db2b3e831e9de86fdb60d91ef6711185bc51
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-08-26 11:57:42 -0400

    added data symlink to .gitignore

--- src/asteroid_traj.py
+++ src/asteroid_traj.py
@@ -6,13 +6,58 @@ Michael S. Emanuel
 Fri Aug 23 16:13:28 2019
 """
 
-# Magic command to run this file using remote kernel
-# %run "/d/IACS/n-body/src/asteroid_traj.py"
-
+# Library imports
+import numpy as np
 import rebound
-from sej_data import make_sim_horizons
+import tqdm
+from typing import List
 
+# ********************************************************************************************************************* 
+def make_sim_horizons(object_names: List[str], horizon_date: str):
+    """Create a new rebound simulation with initial data from the NASA Horizons system"""
+    # Create a simulation
+    sim = rebound.Simulation()
+    
+    # Set units
+    sim.units = ('yr', 'AU', 'Msun')
+    
+    # Add these objects from Horizons
+    sim.add(object_names, date=horizon_date)
+          
+    # Add hashes for the object names
+    for i, particle in enumerate(sim.particles):
+        particle.hash = rebound.hash(object_names[i])
+        
+    # Move to center of mass
+    sim.move_to_com()
+    
+    return sim
 
+# ********************************************************************************************************************* 
+def make_archive(fname_archive, sim, n_years: int, sample_freq: int):
+    """Create a rebound simulation archive and save it to disk"""
+    # Number of samples including both start and end
+    N = n_years*sample_freq + 1
+
+    # Set the times for snapshots
+    ts = np.linspace(0.0, n_years, N, dtype=np.float32)
+
+    # Integrate the simulation up to tmax
+    for i, t in tqdm(enumerate(ts)):
+        # Integrate to the current time step with an exact finish time
+        sim.integrate(t, exact_finish_time=1)
+        # Save a snapshot to the archive file
+        sim.simulationarchive_snapshot(filename=fname_archive)
+
+    # print(f'Created simulation archive in {fname_archive} in {elapsed} seconds.') 
+
+    # Load the updated simulation archive
+    sa = rebound.SimulationArchive(fname_archive)
+    
+    # Return the simulation archive
+    return sa
+
+# ********************************************************************************************************************* 
 # Create simulation using horizons
 sim = rebound.Simulation()
 object_names = ['Sun', 'Ceres']
