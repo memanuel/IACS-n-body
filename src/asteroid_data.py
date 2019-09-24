81679483669c7157d17f12136f8459d99f4d8a1f
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-09-24 12:16:00 -0400

    solar test / tf.data.Dataset from asteroid traj in progress

--- src/asteroid_data.py
+++ src/asteroid_data.py
@@ -22,68 +22,86 @@ from asteroids import load_data, load_sim_np
 # DataFrame of asteroid snapshots
 ast_elt = load_data()
 
+# ********************************************************************************************************************* 
+def make_dataset_one_file(n0: int, n1: int) -> tf.data.Dataset:
+    """
+    Wrap the data in one file of asteroid trajectory data into a TF Dataset
+    INPUTS:
+        n0: the first asteroid in the file, e.g. 0
+        n1: the last asteroid in the file (exclusive), e.g. 1000
+    OUTPUTS:
+        ds: a tf.data.Dataset object for this 
+    """
+    # selected data type for TF tensors
+    dtype = np.float32
+    
+    # Name of the numpy archive
+    fname_np: str = f'../data/asteroids/sim_asteroids_n_{n0:06}_{n1:06}.npz'
+    
+    # The full array of positions and velocities
+    q, v, elts, catalog = load_sim_np(fname_np=fname_np)
+
+    # The object names
+    object_names = catalog['object_names']
+
+    # The snapshot times; offset to start time t0=0
+    ts = catalog['ts'].astype(dtype)
+    
+    # mask for selected asteroids
+    mask = (n0 <= ast_elt.Num) & (ast_elt.Num < n1)
+    
+    # count of selected asteroids
+    # asteroid_names = list(ast_elt.Name[mask].to_numpy())
+    N_ast: int = np.sum(mask)
+    # offset for indexing into asteroids; the first [10] objects are sun and planets
+    ast_offset: int = len(object_names) - N_ast
+
+    # shrink down q and v to slice with asteroid data only; convert to selected data type
+    q = q[:, ast_offset:, :].astype(dtype)
+    v = v[:, ast_offset:, :].astype(dtype)
+
+    # swap axes for time step and body number; TF needs inputs and outputs to have same number of samples
+    # this means that inputs and outputs must first be indexed by asteroid number, then time time step
+    q = np.swapaxes(q, 0, 1)
+    v = np.swapaxes(v, 0, 1)
+
+    # dict with inputs   
+    inputs = {
+        'a': ast_elt.a[mask].to_numpy().astype(dtype),
+        'e': ast_elt.e[mask].to_numpy().astype(dtype),
+        'inc': ast_elt.inc[mask].to_numpy().astype(dtype),
+        'Omega': ast_elt.Omega[mask].to_numpy().astype(dtype),
+        'omega': ast_elt.omega[mask].to_numpy().astype(dtype),
+        'f': ast_elt.f[mask].to_numpy().astype(dtype),
+        'epoch': ast_elt.epoch_mjd[mask].to_numpy().astype(dtype),
+        'ts': np.tile(ts, reps=(N_ast,1,))
+    }
+    
+    # dict with outputs
+    outputs = {
+        'q': q,
+        'v': v,
+    }
+
+    # Build and return the dataset
+    ds = tf.data.Dataset.from_tensor_slices((inputs, outputs))    
+    return ds
+
+# ********************************************************************************************************************* 
 # Start and end time of simulation
-dt0: datetime = datetime(2000, 1, 1)
-dt1: datetime = datetime(2040, 12, 31)
-
-# Load the simulation archive for the first 1000 asteroids
-n0: int = 0
-n1: int = 1000
-
-# Name of the numpy archive
-fname_np: str = f'../data/asteroids/sim_asteroids_n_{n0:06}_{n1:06}.npz'
-
-# The full array of positions and velocities
-q, v, elts, catalog = load_sim_np(fname_np=fname_np)
-# The object names
-object_names = catalog['object_names']
-# The snapshot times; offset to start time t0=0
-ts = catalog['ts']
-epochs = catalog['epochs']
-
-# mask for selected asteroids
-mask = (n0 <= ast_elt.Num) & (ast_elt.Num < n1)
-
-# names of selected asteroids
-asteroid_names = list(ast_elt.Name[mask].to_numpy())
-N_ast: int = np.sum(mask)
-
-# epoch as MJD of selected asteroids
-# epoch_mjd = ast_elt.epoch_mjd[mask].to_numpy()
-# epoch as a datetime
-# epoch_dt = [mjd_to_datetime(mjd) for mjd in epoch_mjd]
+# dt0: datetime = datetime(2000, 1, 1)
+# dt1: datetime = datetime(2040, 12, 31)
 
 # Start and end dates as mjd
 # t0: float = datetime_to_mjd(dt0)
 # t1: float = datetime_to_mjd(dt1)
 
-# Range of desired times for position output
-# ts = arange_inc(t0, t1)
 
-# dict with inputs
-orb_a = ast_elt.a[mask].to_numpy()
-orb_e = ast_elt.e[mask].to_numpy()
-orb_inc = ast_elt.inc[mask].to_numpy()
-orb_Omega = ast_elt.Omega[mask].to_numpy()
-orb_omega = ast_elt.omega[mask].to_numpy()
-orb_f = ast_elt.f[mask].to_numpy()
+# Load the simulation archive for the first 1000 asteroids
+n0: int = 0
+n1: int = 1000
 
+ds = make_dataset_one_file(n0=n0, n1=n1)
 
-inputs = {
-    'a': ast_elt.a[mask].to_numpy(),
-    'e': ast_elt.e[mask].to_numpy(),
-    'inc': ast_elt.inc[mask].to_numpy(),
-    'Omega': ast_elt.Omega[mask].to_numpy(),
-    'omega': ast_elt.omega[mask].to_numpy(),
-    'f': ast_elt.f[mask].to_numpy(),
-    'epoch': ast_elt.epoch_mjd[mask].to_numpy(),
-    'ts': np.tile(ts, reps=(N_ast,1,))
-}
-
-# dict with outputs
-outputs = {
-    'q': np.swapaxes(q, 0, 1),
-    'v': np.swapaxes(v, 0, 1),
-}
-
-ds = tf.data.Dataset.from_tensor_slices((inputs, inputs))
+# one item from batch
+batch_in, batch_out = list(ds.take(1))[0]
