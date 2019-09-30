d2ab551594923288bd37da50cb8c77f636ea374b
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-09-30 17:08:09 -0400

    asteroid traj / tf dataset in progress

--- src/asteroid_data.py
+++ src/asteroid_data.py
@@ -87,6 +87,75 @@ def make_dataset_one_file(n0: int, n1: int) -> tf.data.Dataset:
     ds = tf.data.Dataset.from_tensor_slices((inputs, outputs))    
     return ds
 
+
+# ********************************************************************************************************************* 
+# https://www.tensorflow.org/tutorials/load_data/tfrecord
+def _bytes_feature(value):
+  """Returns a bytes_list from a string / byte."""
+  if isinstance(value, type(tf.constant(0))):
+    value = value.numpy() # BytesList won't unpack a string from an EagerTensor.
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+def _float_feature(value):
+  """Returns a float_list from a float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=[value]))
+
+def _int64_feature(value):
+  """Returns an int64_list from a bool / enum / int / uint."""
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def _float_vec_feature(values):
+  """Returns a float_list from a numpy array of float / double."""
+  return tf.train.Feature(float_list=tf.train.FloatList(value=values))
+
+# ********************************************************************************************************************* 
+def serialize_ast_traj(it):
+    """Serialize one asteroid trajectory to a proto message"""
+    # Unpack inputs and outputs from iterator
+    inputs, outputs = next(it)
+    
+    # Unpack inputs (orbital elements)
+    a = inputs['a']
+    e = inputs['e']
+    inc = inputs['inc']
+    Omega = inputs['Omega']
+    omega = inputs['omega']
+    f = inputs['f']
+    ts = inputs['ts']
+    
+    # Unpack outputs (position and velocity)
+    q = outputs['q']
+    v = outputs['v']
+    
+    # Dictionary mapping feature names to date types compatible with tf.Example
+    feature = {
+        'a': _float_feature(a),
+        'e': _float_feature(e),
+        'inc': _float_feature(inc),
+        'Omega': _float_feature(Omega),
+        'omega': _float_feature(omega),
+        'f': _float_feature(f),
+        'ts': _float_vec_feature(ts),
+        # q and v must be flattened
+        'q': _float_vec_feature(tf.reshape(q, [-1])),
+        'v': _float_vec_feature(tf.reshape(v, [-1])),
+    }
+    
+    # create a message using tf.train.Example
+    proto_msg = tf.train.Example(features=tf.train.Features(feature=feature))
+    return proto_msg.SerializeToString()
+
+def deserialize_ast_traj(proto_msg):
+    """Deserialize an asteroid trajectory stored as a proto message"""
+    pass
+
+# ********************************************************************************************************************* 
+def map_func(n: int):
+    batch_size: int = 1000
+    n0 = batch_size*n
+    n1 = n0 + batch_size
+    return make_dataset_one_file(n0, n1)
+
 # ********************************************************************************************************************* 
 # Start and end time of simulation
 # dt0: datetime = datetime(2000, 1, 1)
@@ -96,12 +165,32 @@ def make_dataset_one_file(n0: int, n1: int) -> tf.data.Dataset:
 # t0: float = datetime_to_mjd(dt0)
 # t1: float = datetime_to_mjd(dt1)
 
-
 # Load the simulation archive for the first 1000 asteroids
 n0: int = 0
 n1: int = 1000
 
-ds = make_dataset_one_file(n0=n0, n1=n1)
+if 'ds' not in globals():
+    ds = make_dataset_one_file(n0=n0, n1=n1)
 
 # one item from batch
 batch_in, batch_out = list(ds.take(1))[0]
+
+if 'ds1' not in globals():
+    ds1 = make_dataset_one_file(n0=0, n1=1000)
+    ds2 = make_dataset_one_file(n0=1000, n1=2000)
+    # combined
+    ds_concat = ds1.concatenate(ds2)
+
+# ds1 = map_func(0)
+# ds2 = map_func(0)
+# idxs = tf.data.Dataset.from_tensor_slices(np.arange(542))
+idxs = tf.data.Dataset.from_tensor_slices(np.arange(8))
+
+# iterator for the dataset
+it = iter(ds)
+
+# serialize one example
+msg = serialize_ast_traj(it)
+
+# recover example
+example = tf.train.Example.FromString(msg)
