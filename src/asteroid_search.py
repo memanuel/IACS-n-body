343afeb8bdbe2c9b282d1caa5dc5e7bceda94920
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-10-21 15:47:23 -0400

    renamed asteroid_search.py

--- src/asteroid_search.py
+++ src/asteroid_search.py
@@ -45,23 +45,30 @@ n1: int = 100
 ds = make_synthetic_obs_dataset(n0=n0, n1=n1)
 # Get reference times
 batch_in, batch_out = list(ds.take(1))[0]
-ts = batch_in['ts'][0]
-# t, u, ast_num = make_synthetic_obs_tensors(n0=n0, n1=n1)
-# traj_size = t.shape[0]
+ts = batch_in['t']
 
-# Model predicting asteroid direction
-model = make_model_ast_dir(ts=ts)
+# Model predicting asteroid direction with batch size 1
+model_1 = make_model_ast_dir(ts=ts, batch_size=1)
+model_64 = make_model_ast_dir(ts=ts, batch_size=64)
+model = model_64
 
-# Values to try
+# Values to try: first 64 asteroids
 ast_num = 1
-a = ast_elt.a[ast_num]
-e = ast_elt.e[ast_num]
-inc = ast_elt.inc[ast_num]
-Omega = ast_elt.Omega[ast_num]
-omega = ast_elt.omega[ast_num]
-f = ast_elt.f[ast_num]
-epoch = ast_elt.epoch_mjd[ast_num]
+dtype = np.float32
+n0: int = 1
+n1: int = 64
+mask = (n0 <= ast_elt.Num) & (ast_elt.Num <= n1)
 
+# Make input batch
+a = ast_elt.a[mask].astype(dtype).to_numpy()
+e = ast_elt.e[mask].astype(dtype).to_numpy()
+inc = ast_elt.inc[mask].astype(dtype).to_numpy()
+Omega = ast_elt.Omega[mask].astype(dtype).to_numpy()
+omega = ast_elt.omega[mask].astype(dtype).to_numpy()
+f = ast_elt.f[mask].astype(dtype).to_numpy()
+epoch = ast_elt.epoch_mjd[mask].astype(dtype).to_numpy()
+
+# Wrap inputs
 inputs = {
     'a': a, 
     'e': e, 
@@ -69,5 +76,12 @@ inputs = {
     'Omega': Omega, 
     'omega': omega, 
     'f': f, 
+    'epoch': epoch
 }
 # u_pred = model()
+
+from asteroid_model import make_model_ast_pos
+model_ast_pos = make_model_ast_pos(ts=ts, batch_size=64)
+
+# Get the asteroid position with this model
+q = model_ast_pos(inputs)
