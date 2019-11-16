454bf7e5c4368743ff9fb452436d7340eb1db30e
Michael S. Emanuel <michael.s.emanuel@gmail.com>
2019-11-16 16:41:21 -0500

    OrbitalElements now stores all elements with control variables in range [0,1] where applicable (except unconstrained angles); these also have scale factor 1.0

--- src/asteroid_search.py
+++ src/asteroid_search.py
@@ -37,22 +37,134 @@ space_dims = 3
 ast_elt = load_data_asteroids()
 
 # Range for a
-log_a_min_ = np.log(0.125) 
-log_a_max_ = np.log(64.0)
+a_min_: float = 0.125
+a_max_: float = 64.0
+# log_a_min_: float = np.log(a_min_) 
+# log_a_max_: float = np.log(a_max_)
 
 # Range for e
-e_min_ = 1.0E-6
-e_max_ = 0.999999
-# log_e_min_ = np.log(1.0E-6)
-# log_e_max_ = np.log(0.999999)
+e_min_: float = 2.0**-20
+e_max_: float = 1.0 - e_min_
 
 # Range for resolution parameter R
-log_R_min_ = np.log(np.deg2rad(1.0/3600))
-log_R_max_ = np.log(np.deg2rad(10.0))
+R_min_ = np.deg2rad(1.0/3600)
+R_max_ = np.deg2rad(10.0)
+log_R_min_ = np.log(R_min_)
+log_R_max_ = np.log(R_max_)
 
 # ********************************************************************************************************************* 
 # Custom Layers
 # ********************************************************************************************************************* 
+
+# ********************************************************************************************************************* 
+class OrbitalElements(keras.layers.Layer):
+    """Custom layer to maintain state of candidate orbital elements and resolutions."""
+
+    def __init__(self, elts_np: dict, batch_size: int, R_deg: float, **kwargs):
+        super(OrbitalElements, self).__init__(**kwargs)
+        
+        # Configuration for serialization
+        self.cfg = {
+            'elts_np': elts_np,
+            'batch_size': batch_size,
+            'R_deg': R_deg,
+        }
+        
+        self.batch_size = batch_size
+        self.elts_np = elts_np
+        self.R_deg = R_deg
+        
+        # Control over a_, in range 0.0 to 1.0
+        self.a_min = tf.constant(a_min_, dtype=tf.float32)
+        self.log_a_range = tf.constant(tf.math.log(a_max_) - tf.math.log(a_min_), dtype=tf.float32)
+        self.a_ = tf.Variable(initial_value=self.inverse_a(elts_np['a']), trainable=True, 
+                              constraint=lambda t: tf.clip_by_value(t, 0.0, 1.0), name='a_')
+        
+        # Control over e_, in range e_min to e_max
+        self.e_min = tf.constant(e_min_, dtype=tf.float32)
+        self.e_range = tf.constant(e_max_ - e_min_, dtype=tf.float32)
+        self.e_ = tf.Variable(initial_value=self.inverse_e(elts_np['e']), trainable=False, 
+                              constraint=lambda t: tf.clip_by_value(t, 0.0, 1.0), name='e_')
+        
+        # Control over inc_, in range -pi/2 to pi/2
+        self.inc_min = tf.constant(np.pi/2*(1-2**-20), dtype=tf.float32)
+        self.inc_range = tf.constant(2*self.inc_min, dtype=tf.float32)
+        self.inc_ = tf.Variable(initial_value=self.inverse_inc(elts_np['inc']), trainable=False, 
+                                constraint=lambda t: tf.clip_by_value(t, 0.0, 1.0), name='inc_')
+        
+        # Scale factor for unconstrained angles is 2*pi
+        self.two_pi = tf.constant(2*np.pi, dtype=tf.float32)
+        
+        self.Omega_ = tf.Variable(initial_value=self.inverse_angle(elts_np['Omega']), trainable=False, name='Omega_')
+        self.omega_ = tf.Variable(initial_value=self.inverse_angle(elts_np['omega']), trainable=False, name='omega_')
+        self.f_ = tf.Variable(initial_value=self.inverse_angle(elts_np['f']), trainable=False, name='f_')
+
+        # The epoch is not trainable
+        self.epoch = tf.Variable(initial_value=elts_np['epoch'], trainable=False, name='epoch')
+        
+        # Control of the resolution factor R_, in range 0.0 to 1.0
+        R_init = np.deg2rad(R_deg) * np.ones_like(elts_np['a'])
+        # log_R_init  = np.log(R_init)
+        self.R_min = tf.constant(R_min_, dtype=tf.float32)
+        self.log_R_range = tf.constant(log_R_max_ - log_R_min_, dtype=tf.float32)
+        self.R_ = tf.Variable(initial_value=self.inverse_R(R_init), trainable=False, 
+                              constraint=lambda t: tf.clip_by_value(t, 0.0, 1.0), name='a_')
+        
+    def get_a(self):
+        """Transformed value of a"""
+        return self.a_min * tf.exp(self.a_ * self.log_a_range)
+
+    def inverse_a(self, a):
+        """Inverse transform value of a"""
+        return tf.math.log(a / self.a_min) / self.log_a_range
+
+    def get_e(self):
+        """Transformed value of e"""
+        return self.e_min + self.e_ * self.e_range
+
+    def inverse_e(self, e):
+        """Inverse transform value of e"""
+        return (e - self.e_min) / self.e_range
+
+    def get_inc(self):
+        """Transformed value of inc"""
+        return self.inc_min + self.inc_ * self.inc_range
+
+    def inverse_inc(self, inc):
+        """Inverse transform value of inc"""
+        return (inc - self.inc_min) / self.inc_range
+
+    def get_angle(self, angle_):
+        """Forward transform of an unconstrained angle variable (Omega, omega, f)"""
+        return self.two_pi * angle_
+
+    def inverse_angle(self, angle):
+        """Forward transform of an unconstrained angle variable (Omega, omega, f)"""
+        return angle / self.two_pi
+
+    def get_R(self):
+        """Transformed value of R"""
+        return self.R_min * tf.exp(self.R_ * self.log_R_range)
+
+    def inverse_R(self, R):
+        """Inverse transform value of R"""
+        return tf.math.log(R / self.R_min) / self.log_R_range
+
+    def call(self, inputs):
+        """Return the current orbital elements and resolution"""
+        # print(f'type(inputs)={type(inputs)}.')
+        # Transform a, e, and R from log to linear
+        a = self.get_a()
+        e = self.get_e()
+        inc = self.get_inc()
+        Omega = self.get_angle(self.Omega_)
+        omega = self.get_angle(self.omega_)
+        f = self.get_angle(self.f_)
+        R = self.get_R()
+        return a, e, inc, Omega, omega, f, self.epoch, R
+
+    def get_config(self):
+        return self.cfg
 
 # ********************************************************************************************************************* 
 class DirectionDifference(keras.layers.Layer):
@@ -187,64 +299,11 @@ class TrajectoryScore(keras.layers.Layer):
         # objective = scale_factor * (raw_score - mu_factor * mu) * tf.math.pow(sigma, -sigma_power)
         objective = scale_factor * (raw_score - self.alpha * mu - self.beta * sigma2)
 
+        # Add the loss function
+        self.add_loss(-tf.reduce_sum(objective))
+
         # Return both the raw and t scores
         return raw_score, mu, sigma2, objective
-
-    def get_config(self):
-        return self.cfg
-
-# ********************************************************************************************************************* 
-class OrbitalElements(keras.layers.Layer):
-    """Custom layer to maintain state of candidate orbital elements and resolutions."""
-
-    def __init__(self, elts_np: dict, batch_size: int, R_deg: float, **kwargs):
-        super(OrbitalElements, self).__init__(**kwargs)
-        
-        # Configuration for serialization
-        self.cfg = {
-            'elts_np': elts_np,
-            'batch_size': batch_size,
-            'R_deg': R_deg,
-        }
-        
-        self.batch_size = batch_size
-        self.elts_np = elts_np
-        self.R_deg = R_deg
-        self.log_a = tf.Variable(initial_value=np.log(elts_np['a']), trainable=True, 
-                                 constraint=lambda t: tf.clip_by_value(t, log_a_min_, log_a_max_), name='log_a')
-        # self.log_e = tf.Variable(initial_value=np.log(elts_np['e']), trainable=True, 
-        #                         constraint=lambda t: tf.clip_by_value(t, log_e_min_, log_e_max_), name='log_e')
-        self.e = tf.Variable(initial_value=elts_np['e'], trainable=False, 
-                             constraint=lambda t: tf.clip_by_value(t, e_min_, e_max_), name='e')
-        self.inc = tf.Variable(initial_value=elts_np['inc'], trainable=False, name='inc')
-        self.Omega = tf.Variable(initial_value=elts_np['Omega'], trainable=False, name='Omega')
-        self.omega = tf.Variable(initial_value=elts_np['omega'], trainable=False, name='omega')
-        self.f = tf.Variable(initial_value=elts_np['f'], trainable=False, name='f')
-
-        # The epoch is not trainable
-        self.epoch = tf.Variable(initial_value=elts_np['epoch'], trainable=False, name='epoch')
-        
-        # The control of the log of the resolution factor between R_min and R_max
-        R_init= np.deg2rad(R_deg) * np.ones_like(elts_np['a'])
-        log_R_init  = np.log(R_init)
-        self.log_R = tf.Variable(initial_value=log_R_init, trainable=False, 
-                                 constraint=lambda t: tf.clip_by_value(t, log_R_min_, log_R_max_), name='log_R')
-        
-    def get_a(self):
-        """Transformed value of a"""
-        return tf.exp(self.log_a)
-
-    def get_R(self):
-        """Transformed value of R"""
-        return tf.exp(self.log_R)
-
-    def call(self, inputs):
-        """Return the current orbital elements and resolution"""
-        # print(f'type(inputs)={type(inputs)}.')
-        # Transform a, e, and R from log to linear
-        a = self.get_a()
-        R = self.get_R()
-        return a, self.e, self.inc, self.Omega, self.omega, self.f, self.epoch, R
 
     def get_config(self):
         return self.cfg
@@ -339,7 +398,7 @@ def make_model_asteroid_search(ts: tf.Tensor,
     model.score = score_layer
     
     # Add custom loss; negative of the total objective function
-    model.add_loss(-tf.reduce_sum(objective))
+    # model.add_loss(-tf.reduce_sum(objective))
 
     return model
 
@@ -392,15 +451,19 @@ def report_model_attribute(att: np.array, mask_good: np.array, att_name: str):
     att_b = att[mask_bad]
     mean_g = np.mean(att_g, axis=0)
     mean_b = np.mean(att_b, axis=0)
+    mean_a = np.mean(att, axis=0)
     std_g = np.std(att_g, axis=0)
     std_b = np.std(att_b, axis=0)
+    std_a = np.std(att, axis=0)
 
     print(f'\nMean & std {att_name} by Category:')
     print(f'Good: {mean_g:8.2f} +/- {std_g:8.2f}')
     print(f'Bad:  {mean_b:8.2f} +/- {std_b:8.2f}')
+    print(f'All:  {mean_a:8.2f} +/- {std_a:8.2f}')
+    
 
 # ********************************************************************************************************************* 
-def report_model(model, R_deg, mask_good, batch_size, steps, elts_true):
+def report_model(model, R_deg, mask_good, batch_size, steps, elts_true, display=True):
     """Report summary of model on good and b"""
 
     mask_bad = ~mask_good
@@ -453,23 +516,24 @@ def report_model(model, R_deg, mask_good, batch_size, steps, elts_true):
     mean_err_g = np.mean(elt_err_g[0:6], axis=0)
     mean_err_b = np.mean(elt_err_b[0:6], axis=0)
 
-    # Report errors in orbital elements
-    print(f'\nMean Loss per batch =  {loss:8.0f}')
-    print(f'Mean Objective per elt = {obj_per_elt:8.0f}')
-    print(f'\nError in orbital elements:')
-    print(f'Good: {mean_err_g[0]:5.2e},  {mean_err_g[1]:5.2e}, {mean_err_g[2]:5.2e}, '
-          f'{mean_err_g[0]:5.2e},  {mean_err_g[1]:5.2e}, {mean_err_g[2]:5.2e}, ')
-    print(f'Bad : {mean_err_b[0]:5.2e},  {mean_err_b[1]:5.2e}, {mean_err_b[2]:5.2e}, '
-          f'{mean_err_b[0]:5.2e},  {mean_err_b[1]:5.2e}, {mean_err_b[2]:5.2e}, ')
+    if display:
+        # Report errors in orbital elements
+        # print(f'\nMean Loss per batch =  {loss_mean:8.0f}')
+        print(f'\nMean Objective per elt = {obj_per_elt:8.0f}')
+        print(f'\nError in orbital elements:')
+        print(f'Good: {mean_err_g[0]:5.2e},  {mean_err_g[1]:5.2e}, {mean_err_g[2]:5.2e}, '
+              f'{mean_err_g[0]:5.2e},  {mean_err_g[1]:5.2e}, {mean_err_g[2]:5.2e}, ')
+        print(f'Bad : {mean_err_b[0]:5.2e},  {mean_err_b[1]:5.2e}, {mean_err_b[2]:5.2e}, '
+              f'{mean_err_b[0]:5.2e},  {mean_err_b[1]:5.2e}, {mean_err_b[2]:5.2e}, ')
     
-    # Report effective observations, mu, sigma, and t_score
-    report_model_attribute(raw_score, mask_good, 'Raw Score')
-    report_model_attribute(mu, mask_good, 'Mu')
-    report_model_attribute(eff_obs, mask_good, 'Effective Observations')
-    report_model_attribute(sigma, mask_good, 'Sigma')
-    report_model_attribute(t_score, mask_good, 't_score')
-    report_model_attribute(objective, mask_good, 'Objective Function')
-    report_model_attribute(d_R, mask_good, 'Change in resolution R')
+        # Report effective observations, mu, sigma, and t_score    
+        # report_model_attribute(raw_score, mask_good, 'Raw Score')
+        # report_model_attribute(mu, mask_good, 'Mu')
+        report_model_attribute(eff_obs, mask_good, 'Effective Observations')
+        # report_model_attribute(sigma, mask_good, 'Sigma')
+        report_model_attribute(t_score, mask_good, 't_score')
+        report_model_attribute(objective, mask_good, 'Objective Function')
+        # report_model_attribute(d_R, mask_good, 'Change in resolution R')
 
     return scores, elt_err
 
@@ -487,21 +551,21 @@ def report_training_progress(d_scores, d_elt_err, d_R):
     # Calculations
     d_eff_obs = d_raw_score - d_mu
 
-    report_model_attribute(d_raw_score, mask_good, 'Change in Raw Score')
-    report_model_attribute(d_eff_obs, mask_good, 'Change in Effective Observations')
-    report_model_attribute(d_t_score, mask_good, 'Change in t_score')
+    # report_model_attribute(d_raw_score, mask_good, 'Change in Raw Score')
+    # report_model_attribute(d_eff_obs, mask_good, 'Change in Effective Observations')
+    # report_model_attribute(d_t_score, mask_good, 'Change in t_score')
     report_model_attribute(d_objective, mask_good, 'Change in Objective Function')
-    report_model_attribute(d_R, mask_good, 'Change in resolution R')
+    # report_model_attribute(d_R, mask_good, 'Change in resolution R')
 
     # Changes in element errors and R
     d_err_g = np.mean(d_elt_err[mask_good], axis=0)
     d_err_b = np.mean(d_elt_err[mask_bad], axis=0)
     
-    print(f'\nChange in Orbital Element error by Category:')
-    print(f'd_err_g: {d_err_g[0]:+5.2e},  {d_err_g[1]:+5.2e}, {d_err_g[2]:+5.2e}, '
-          f'{d_err_g[3]:+5.2e},  {d_err_g[4]:+5.2e}, {d_err_g[5]:+5.2e}, ')
-    print(f'd_err_b: {d_err_b[0]:+5.2e},  {d_err_b[1]:+5.2e}, {d_err_b[2]:+5.2e}, '
-          f'{d_err_b[4]:+5.2e},  {d_err_b[5]:+5.2e}, {d_err_b[5]:+5.2e}, ')
+    #    print(f'\nChange in Orbital Element error by Category:')
+    #    print(f'd_err_g: {d_err_g[0]:+5.2e},  {d_err_g[1]:+5.2e}, {d_err_g[2]:+5.2e}, '
+    #          f'{d_err_g[3]:+5.2e},  {d_err_g[4]:+5.2e}, {d_err_g[5]:+5.2e}, ')
+    #    print(f'd_err_b: {d_err_b[0]:+5.2e},  {d_err_b[1]:+5.2e}, {d_err_b[2]:+5.2e}, '
+    #          f'{d_err_b[4]:+5.2e},  {d_err_b[5]:+5.2e}, {d_err_b[5]:+5.2e}, ')
 
 # ********************************************************************************************************************* 
 # Dataset of observations: synthetic data on first 1000 asteroids
@@ -559,9 +623,14 @@ model = make_model_asteroid_search(ts=ts,
                                    R_deg = R_deg,
                                    q_cal=q_cal)
 # Use Adam optimizer with gradient clipping
-# opt = keras.optimizers.Adam(learning_rate=0.0005, clipvalue=1.0)
-opt = keras.optimizers.Adam(learning_rate=1.0e-6, clipvalue=1.0)
-# opt = keras.optimizers.Adadelta(learning_rate=0.001, clipvalue=1.0)
+# opt = keras.optimizers.Adam(learning_rate=1.0e-6, epsilon=1.0, amsgrad=True, clipvalue=1.0, )
+# opt = keras.optimizers.SGD(learning_rate=1.0e-9, clipvalue=1.0E-9)
+# opt = keras.optimizers.Adadelta(learning_rate=1.0e-4, rho=0.999, epsilon=1.0E-10, clipnorm=1.0)
+# opt = keras.optimizers.Adadelta(learning_rate=1.0e-4, rho=0.999, epsilon=1.0E-10, clipnorm=1.0)
+opt = keras.optimizers.Adamax(learning_rate=1.0e-6, beta_1=0.999, beta_2=0.999, 
+                              epsilon=1.0E-7, clipnorm=1.0)
+opt = keras.optimizers.Adam(learning_rate=1.0e-7, beta_1=0.900, beta_2=0.999,
+                            epsilon=1.0E-7, amsgrad=False, clipnorm=1.0)
 model.compile(optimizer=opt)
 
 # Clone model before training
@@ -573,6 +642,10 @@ model0 = make_model_asteroid_search(ts=ts,
                                     time_batch_size=time_batch_size,
                                     R_deg = R_deg,
                                     q_cal=q_cal)
+model0.compile(optimizer=opt)
+
+# Set display for report_model
+display = True
 
 # Report losses before training
 print(f'Processed first {n1} asteroids; seeded with first 64. Perturbed 33-65.')
@@ -581,11 +654,11 @@ print(f'\nModel before training:')
 pred0 = model.predict_on_batch(ds)
 elts0, R0, u_pred0, z0, _ = pred0
 scores0, elt_err0 = report_model(model=model, R_deg=R_deg, mask_good=mask_good, 
-                                 batch_size=elt_batch_size, steps=steps, elts_true=elts_true)
+                                 batch_size=elt_batch_size, steps=steps, elts_true=elts_true, display=display)
 
 # Get gradients on entire data set
 with tf.GradientTape(persistent=True) as gt:
-    gt.watch([model.elements.e, model.elements.inc, model.elements.log_R])
+    gt.watch([model.elements.e_, model.elements.inc_, model.elements.R_])
     pred = model.predict_on_batch(ds.take(traj_size))
     # pred = model.predict_on_batch(ds.take(traj_size))
     elts, R, u_pred, z, scores = pred
@@ -594,20 +667,21 @@ with tf.GradientTape(persistent=True) as gt:
     sigma2 = scores[:, 2]
     objective = scores[:, 3]
     loss = tf.reduce_sum(-objective)
-dL_da = gt.gradient(loss, model.elements.log_a) / steps
-dL_de = gt.gradient(loss, model.elements.e) / steps
-dL_dinc = gt.gradient(loss, model.elements.inc) / steps
+dL_da = gt.gradient(loss, model.elements.a_) / steps
+dL_de = gt.gradient(loss, model.elements.e_) / steps
+dL_dinc = gt.gradient(loss, model.elements.inc_) / steps
 # dL_dOmega = gt.gradient(loss, model.elements.Omega) / steps
 # dL_domega = gt.gradient(loss, model.elements.omega) / steps
 # dL_df = gt.gradient(loss, model.elements.f) / steps
-dL_dR = gt.gradient(loss, model.elements.log_R) / steps
+dL_dR = gt.gradient(loss, model.elements.R_) / steps
 del gt
 
 # Train model
-step_multiplier = 4
+step_multiplier = 1
 steps_per_epoch = steps*step_multiplier
 steps_per_epoch = 1
-hist = model.fit(ds, epochs=1, steps_per_epoch=steps_per_epoch)
+epochs = 1
+hist = model.fit(ds, epochs=epochs, steps_per_epoch=steps_per_epoch)
 pred = model.predict_on_batch(ds)
 elts, R, u_pred, z, scores = pred
 elt_err = np.abs(elts - elts_true)
@@ -615,7 +689,7 @@ elt_err = np.abs(elts - elts_true)
 # Report results
 print(f'\nModel after training:')
 scores, elt_err = report_model(model=model, R_deg=R_deg, mask_good=mask_good, 
-                               batch_size=elt_batch_size, steps=steps, elts_true=elts_true)
+                               batch_size=elt_batch_size, steps=steps, elts_true=elts_true, display=display)
 # Unpack scores after training
 raw_score = scores[:,0]
 mu = scores[:,1]
@@ -642,6 +716,6 @@ da = orb1_a - orb0_a
 
 # err_a = np.abs(model.elements.get_a() - elts_np['a'])
 
-orb0_log_a = np.log(elts_np['a'])
-orb1_log_a = model.elements.log_a
-d_log_a = orb1_log_a - orb0_log_a
+orb0_a_ = model0.elements.a_
+orb1_a_ = model.elements.a_
+d_a_ = orb1_a_ - orb0_a_
